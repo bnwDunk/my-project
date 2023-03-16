ffe93955e01f3e23158d6b77aac520ee352059be
maipk39196 <maipk123456@gmail.com>
2023-03-16 21:04:41 +0700

    account5

--- Account.py
+++ Account.py
@@ -15,12 +15,14 @@ class Person:
     self.address = address
 
 class Admin(Person):
-  def __init__(self, name, password, email, tel, address):
+  def __init__(self, name, password, email, tel, address, role):
     Person.__init__(self, name, password, email, tel, address)
+    self.role = role
 
 class Customer(Person):
-  def __init__(self, name, password, email, tel, address):
+  def __init__(self, name, password, email, tel, address, role):
     Person.__init__(self, name, password, email, tel, address)
+    self.role = role
     
 #=========================Instant================================
 
@@ -35,24 +37,14 @@ monkey = Account("00006","deef","customer")
 turtle = Account("00007","camew","customer")
 brownie = Account("00008","brown","customer")
 
-#======Person=======
-dunk = Person("dunk", "123456", "dunk@gmail.com", "0611111111", "bangkok")
-first = Person("first", "123456", "first@gmail.com", "0622222222", "bangkok")
-mile = Person("mile", "123456", "mile@gmail.com", "0633333333", "bangkok")
-mai = Person("mai", "123456", "mai@gmail.com", "0644444444", "bangkok")
-thana = Person("thana", "123456", "thana@gmail.com", "0655555555", "bangkok")
-deef = Person("deef", "123456", "deef@gmail.com", "0666666666", "bangkok")
-mew = Person("mew", "123456", "mew@gmail.com", "0677777777", "bangkok")
-brown = Person("brown", "123456", "brown@gmail.com", "0688888888", "bangkok")
-
 #======Admin=======
-ad_dunk = Admin("dunk", "123456", "dunk@gmail.com", "0611111111", "bangkok")
-ad_first = Admin("first", "123456", "first@gmail.com", "0622222222", "bangkok")
-ad_mile = Admin("mile", "123456", "mile@gmail.com", "0633333333", "bangkok")
-ad_mai = Admin("mai", "123456", "mai@gmail.com", "0644444444", "bangkok")
+ad_dunk = Admin("dunk", "123456", "dunk@gmail.com", "0611111111", "bangkok", "admin")
+ad_first = Admin("first", "123456", "first@gmail.com", "0622222222", "bangkok", "admin")
+ad_mile = Admin("mile", "123456", "mile@gmail.com", "0633333333", "bangkok", "admin")
+ad_mai = Admin("mai", "123456", "mai@gmail.com", "0644444444", "bangkok", "admin")
 
 #======Customer=======
-cus_thana = Customer("thana", "123456", "thana@gmail.com", "0655555555", "bangkok")
-cus_deef = Customer("deef", "123456", "deef@gmail.com", "0666666666", "bangkok")
-cus_mew = Customer("mew", "123456", "mew@gmail.com", "0677777777", "bangkok")
-cus_brown = Customer("brown", "123456", "brown@gmail.com", "0688888888", "bangkok")
+cus_thana = Customer("thana", "123456", "thana@gmail.com", "0655555555", "bangkok", "customer")
+cus_deef = Customer("deef", "123456", "deef@gmail.com", "0666666666", "bangkok", "customer")
+cus_mew = Customer("mew", "123456", "mew@gmail.com", "0677777777", "bangkok", "customer")
+cus_brown = Customer("brown", "123456", "brown@gmail.com", "0688888888", "bangkok", "customer")
